6067f895e5f885ce7d1e9e225bf21d84d551727d
lilinsen <lilinsen@jd.com>
2019-11-05 20:14:19 +0800

    验证权限

--- server/router/manager.js
+++ server/router/manager.js
@@ -1,26 +1,33 @@
 const Router = require('koa-router')
 const User = require('../monk/user')
+const power =require('./power');
 const users = new Router()
+const noPower = {
+    code:0,
+    msg:'权限不足'
+}
 // 管理 用户
 users.post('/manager/user', async (ctx) => {
-    if(ctx.session.user.type === 'super'){
+    if(power.reg()){
         const data = ctx.request.body    
         const param = Object.assign(data,{semailCode:ctx.session.emailCode})       
         ctx.body = await User.add(param)
     }else{
-        ctx.body = {
-            code:0,
-            msg:'权限不足'
-        }
+        ctx.body = noPower;
     }
     
     //  如果已经登陆   
 })
 //  查看用户 申请 edu
 users.post('/manager/useApply', async (ctx) => {
-    const data = ctx.request.body    
-    const param = Object.assign(data,{semailCode:ctx.session.emailCode})       
-    ctx.body = await User.add(param)
+    if(power.reg()){
+        const data = ctx.request.body    
+        const param = Object.assign(data,{semailCode:ctx.session.emailCode})       
+        ctx.body = await User.add(param)
+    }else{
+        ctx.body = noPower;
+    }
+    
     //  如果已经登陆   
 })
 module.exports = users
